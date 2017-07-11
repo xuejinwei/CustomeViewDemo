38c31628e42a5f3e009bcf3e702d34e9bbffac97
xuejinwei <xuejinwei@outlook.com>
2017-07-11 11:24:24 +0800

    添加打印屏幕参数

--- app/src/main/java/com/xuejinwei/customeviewdemo/MainActivity.java
+++ app/src/main/java/com/xuejinwei/customeviewdemo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.xuejinwei.customeviewdemo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +16,7 @@ import com.xuejinwei.customeviewdemo.Activity.ViewGroupActivity;
 import com.xuejinwei.customeviewdemo.Path.PathActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private String TAG = "MainActivity";
     private Button mBtnNumberProgressbar, mBtnClork, mBtnCredit, mBtnPie, mBtnBezier, mBtnViewGroup;
 
     @Override
@@ -33,6 +36,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mBtnPie.setOnClickListener(this);
         mBtnBezier.setOnClickListener(this);
         mBtnViewGroup.setOnClickListener(this);
+
+        printScreenLog();
 
     }
 
@@ -59,5 +64,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             default:
                 break;
         }
+    }
+
+    public void printScreenLog() {
+        // 获取屏幕密度(在Context环境下，比如在Activity中执行这些代码或者传入Context对象去获得DisplayMetrics）
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+
+        float density = dm.density;        // 屏幕密度（像素比例：0.75/1.0/1.5/2.0,3.0,4等）
+        int densityDPI = dm.densityDpi;        // 屏幕密度(这里是系统密度）（每寸像素：120/160/240/320,480,640等）
+        float xdpi = dm.xdpi; //这两个是水平和垂直方向上的dpi，如果要计算水平和垂直方向上的实际dp值，可以通过后面`屏幕的宽高差异`中的内容计算。不过你倒是可以通过这两个值大致计算屏幕的宽和高的英寸
+        float ydpi = dm.ydpi;
+        int screenWidth = dm.widthPixels;        // 屏幕像素宽（像素，如：480px）
+        int screenHeight = dm.heightPixels;        // 屏幕像素高（像素，如：800px）
+        Log.i(TAG, "  DisplayMetrics: " + "xdpi=" + xdpi + "; ydpi=" + ydpi);
+        Log.i(TAG, "  DisplayMetrics: " + "density=" + density + "; densityDPI=" + densityDPI);
+        Log.i(TAG, "  DisplayMetrics" + "screenWidth=" + screenWidth + "; screenHeight=" + screenHeight);
     }
 }
